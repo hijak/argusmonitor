4f2a27eb362bbe3e17a38811bb045c06c93a9242
Plutus <plutus.ghost@gmail.com>
2026-03-30 16:19:18 +0100

    Remove extracted website, plugin directory, and operator repos from main monorepo

--- website/src/lib/site.ts
+++
@@ -1,7 +0,0 @@
-export const GITHUB_URL = "https://github.com/hijak/vordr";
-export const DOCS_URL = "http://10.13.37.9:8789";
-export const DEMO_URL = "mailto:plutus.ghost@gmail.com?subject=Vordr%20Demo";
-export const START_URL = GITHUB_URL;
-export const APP_URL = "http://10.13.37.9:8080";
-export const WEBSITE_URL = "http://10.13.37.9:8788";
-export const PLUGIN_DIRECTORY_URL = "http://10.13.37.9:8790";
